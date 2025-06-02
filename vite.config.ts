5983263d829550fae177c8542b1632fcbdf1f7ca
shane-technomics <sstumvoll@technomics.net>
2025-06-02 10:15:15 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-    const repoName = process.env.GITHUB_REPOSITORY
-    ? process.env.GITHUB_REPOSITORY.split('/')[1]
-    : '';
 
-  // Set base to /<repoName>/ for GitHub Pages, otherwise / for local dev or other deployments
-  const base = process.env.GITHUB_REPOSITORY ? `/${repoName}/` : '/';
-
-  base: base,
   server: {
     host: "::",
     port: 8080,
